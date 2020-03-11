72c4b7b283255759584bd033722b5b1c900250cb
Alexandru <rammazzoti2000@gmail.com>
2020-03-11 16:06:39 +0100

    added my_select method

--- enumerables.rb
+++ enumerables.rb
@@ -20,4 +20,12 @@ module Enumerable
     end
   end
 
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    select = []
+    my_each { |elem| select << elem if yield(elem) }
+    select
+  end
+
 end
