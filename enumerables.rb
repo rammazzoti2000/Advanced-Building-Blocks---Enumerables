146cc02f2b468d8a5a2323045a4ca5199f07becb
Alexandru <rammazzoti2000@gmail.com>
2020-03-11 16:05:06 +0100

    added my_each method

--- enumerables.rb
+++ enumerables.rb
@@ -1,15 +1,13 @@
+module Enumerable
+  def my_each
+    return to_enum(:my_each) unless block_given?
 
-
-
-  def my_each(num)
     i = 0
-    while i < num.size
-      yield num[i]
+    size = self.size
+    while i < size
+      yield self[i]
       i += 1
     end
-  end
+  end  
 
-
-
-my_each([1,2,3]) do |num| puts num * 2 end
-
+end
