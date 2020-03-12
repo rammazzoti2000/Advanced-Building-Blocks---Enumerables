13e980d6913f722e4a55f35ad3db196335a3803b
Alex <bangau.alexandru@gmail.com>
2020-03-12 19:35:09 +0100

    optional test-case for my_map
    it should accept a block or a block or both

--- enumerables.rb
+++ enumerables.rb
@@ -79,16 +79,15 @@ module Enumerable
     count
   end
 
-  def my_map(&prc)
-    return to_enum(:my_map) unless block_given?
-
+  def my_map(prc = nil)
+    return enum_for(:map) unless block_given?
+    
     mapped = []
-    my_each do |elem|
-      mapped << prc.call(elem)
-    end
+    my_each { |elem| mapped << prc.call(elem) } if block_given? && prc      
+    my_each { |elem| mapped << yield(elem) } if prc.nil?
     mapped
   end
-
+  
   def my_inject(memo = nil, sym = nil, &prc)
     memo = memo.to_sym if memo.is_a?(String) && !sym && !prc
 
