bb5a681eaa43e3b4871c70fe39945eecf98d2253
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-03-20 16:52:28 +0100

    rspec test for my_each_with_index method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -6,17 +6,32 @@ RSpec.describe Enumerable do
   let(:array) {[1, 2, 3]}
 
   describe "#my_each" do
-    it "returns to enumerator when no block given" do
+    it "should return to enumerator when no block given" do
       expect(array.my_each).to be_a(Enumerator)
       expect(array.my_each.to_a).to eq(array)
     end
 
-    it "returns same as each method when block given" do
+    it "should return same as each method when block given" do
       expect(array.my_each { |elem| puts "return: #{elem}"}).to eq(array.each {|elem| puts "return: #{elem}"})
     end
 
-    it "raises ArgumentError when argument is given" do
-      expect {array.my_each('argument')}.to raise_error(ArgumentError)
+    it "should throw an error if given an argument" do
+      expect { array.my_each('argument') }.to raise_error(ArgumentError)
     end
+  end
+
+  describe "#my_each_with_index" do
+    it "should return to enumerator when no block given" do
+      expect(array.my_each_with_index).to be_a(Enumerator)
+    end
+
+    it "should return same as each_with_index method when block given" do
+      expect(array.my_each_with_index { |elem, index| puts "#{elem} : #{index}"}).to eq(array.each_with_index {|elem, index| puts "#{elem} : #{index}"})
+    end
+
+    it "should throw an error if given an argument" do
+      expect { array.my_each_with_index('argument') }.to raise_error(ArgumentError)
+    end
+
   end
 end
